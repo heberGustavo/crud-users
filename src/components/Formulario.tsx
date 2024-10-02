f557fa157e3dcefa7c39d803d43c124aac0a815f
heberGustavo <heber.gbarbosa@hotmail.com>
2024-10-02 08:06:29 -0300

    Create Form

---
+++ src/components/Formulario.tsx
@@ -0,0 +1,52 @@
+import Cliente from "@/core/Cliente";
+import Entrada from "./Entrada";
+import { useState } from "react";
+import Botao from "./Botao";
+
+interface FormularioProps{
+    cliente: Cliente;
+}
+
+export default function Formulario(props: FormularioProps){
+    const id = props.cliente?.id;
+
+    const [nome, setNome] = useState(props.cliente?.nome ?? "");
+    const [idade, setIdade] = useState(props.cliente?.idade ?? 0);
+
+    return(
+        <div>
+            {id ? (
+                <Entrada 
+                    tipo="text"
+                    somenteLeitura 
+                    texto="Código" 
+                    valor={id}
+                    className="mb-4"
+                />
+            ): false}
+
+            <Entrada 
+                tipo="text" 
+                texto="Nome" 
+                valor={nome}
+                valorMudou={setNome}
+                className="mb-4"
+            />
+            <Entrada 
+                tipo="number" 
+                texto="Idade" 
+                valor={idade}
+                valorMudou={setIdade}
+            />
+
+            <div className="flex justify-end mt-7">
+                <Botao cor="blue" className="mr-2">
+                    {id ? "Alterar" : "Salvar"}
+                </Botao>
+                <Botao cor="gray">
+                    Cancelar
+                </Botao>
+            </div>
+        </div>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Botao from "@/components/Botao";
+import Formulario from "@/components/Formulario";
 import Layout from "@/components/Layout";
 import Tabela from "@/components/Tabela";
 import Cliente from "@/core/Cliente";
@@ -12,14 +13,14 @@ export default function Home() {
     new Cliente('Antonio', 49, '4'),
   ]
 
-  function clienteSelecionado(cliente: Cliente){
+  function clienteSelecionado(cliente: Cliente) {
     console.log(cliente.nome)
   }
 
-  function clienteExcluido(cliente: Cliente){
+  function clienteExcluido(cliente: Cliente) {
     console.log(cliente.idade)
   }
-  
+
   return (
     <main className={`
       flex justify-center items-center h-screen
@@ -28,13 +29,16 @@ export default function Home() {
     `}>
       <Layout titulo="Cadastro Simples">
         <div className="flex justify-end">
-        <Botao cor="green" className="mb-4">Novo Cliente</Botao>
+          <Botao cor="green" className="mb-4">Novo Cliente</Botao>
         </div>
-        <Tabela 
-          clientes={clientes} 
-          clienteSelecionado={clienteSelecionado} 
+
+        <Tabela
+          clientes={clientes}
+          clienteSelecionado={clienteSelecionado}
           clienteExcluido={clienteExcluido}
         />
+
+        <Formulario cliente={clientes[0]} />
       </Layout>
     </main>
   )
