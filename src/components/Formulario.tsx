ee4b58a1b161f0acf05b54ae729242ba2a581a80
heberGustavo <heber.gbarbosa@hotmail.com>
2024-10-02 08:34:49 -0300

    Create actions to buttons

--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -3,50 +3,58 @@ import Entrada from "./Entrada";
 import { useState } from "react";
 import Botao from "./Botao";
 
-interface FormularioProps{
-    cliente: Cliente;
+interface FormularioProps {
+  cliente: Cliente;
+  clienteMudou?: (cliente: Cliente) => void;
+  cancelado?: () => void;
 }
 
-export default function Formulario(props: FormularioProps){
-    const id = props.cliente?.id;
+export default function Formulario(props: FormularioProps) {
+  const id = props.cliente?.id;
 
-    const [nome, setNome] = useState(props.cliente?.nome ?? "");
-    const [idade, setIdade] = useState(props.cliente?.idade ?? 0);
+  const [nome, setNome] = useState(props.cliente?.nome ?? "");
+  const [idade, setIdade] = useState(props.cliente?.idade ?? 0);
 
-    return(
-        <div>
-            {id ? (
-                <Entrada 
-                    tipo="text"
-                    somenteLeitura 
-                    texto="Código" 
-                    valor={id}
-                    className="mb-4"
-                />
-            ): false}
+  return (
+    <div>
+      {id ? (
+        <Entrada
+          tipo="text"
+          somenteLeitura
+          texto="Código"
+          valor={id}
+          className="mb-4"
+        />
+      ) : (
+        false
+      )}
 
-            <Entrada 
-                tipo="text" 
-                texto="Nome" 
-                valor={nome}
-                valorMudou={setNome}
-                className="mb-4"
-            />
-            <Entrada 
-                tipo="number" 
-                texto="Idade" 
-                valor={idade}
-                valorMudou={setIdade}
-            />
+      <Entrada
+        tipo="text"
+        texto="Nome"
+        valor={nome}
+        valorMudou={setNome}
+        className="mb-4"
+      />
+      <Entrada
+        tipo="number"
+        texto="Idade"
+        valor={idade}
+        valorMudou={setIdade}
+      />
 
-            <div className="flex justify-end mt-7">
-                <Botao cor="blue" className="mr-2">
-                    {id ? "Alterar" : "Salvar"}
-                </Botao>
-                <Botao cor="gray">
-                    Cancelar
-                </Botao>
-            </div>
-        </div>
-    )
+      <div className="flex justify-end mt-7">
+        <Botao
+          cor="blue"
+          className="mr-2"
+          onClick={() => props.clienteMudou?.(new Cliente(nome, +idade, id))}
+        >
+          {id ? "Alterar" : "Salvar"}
+        </Botao>
+        <Botao cor="gray" onClick={props.cancelado}>
+          Cancelar
+        </Botao>
+      </div>
+    </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,42 +4,76 @@ import Layout from "@/components/Layout";
 import Tabela from "@/components/Tabela";
 import Cliente from "@/core/Cliente";
 
-export default function Home() {
+import { useState } from "react";
 
+export default function Home() {
   const clientes = [
-    new Cliente('Ana', 22, '1'),
-    new Cliente('Maria', 32, '2'),
-    new Cliente('Joaquim', 50, '3'),
-    new Cliente('Antonio', 49, '4'),
-  ]
+    new Cliente("Ana", 22, "1"),
+    new Cliente("Maria", 32, "2"),
+    new Cliente("Joaquim", 50, "3"),
+    new Cliente("Antonio", 49, "4"),
+  ];
+
+  const [visivel, setVisivel] = useState<"tabela" | "form">("tabela");
+  const [cliente, setCliente] = useState<Cliente>(Cliente.vazio);
+
+  function salvarCliente(cliente: Cliente) {
+    console.log(cliente);
+    setVisivel("tabela");
+  }
+
+  function novoCliente() {
+    setCliente(Cliente.vazio)
+    setVisivel("form");
+  }
+
 
   function clienteSelecionado(cliente: Cliente) {
-    console.log(cliente.nome)
+    setCliente(cliente);
+    setVisivel("form");
   }
 
   function clienteExcluido(cliente: Cliente) {
-    console.log(cliente.idade)
+    console.log(cliente.idade);
   }
 
   return (
-    <main className={`
+    <main
+      className={`
       flex justify-center items-center h-screen
       bg-gradient-to-r from-blue-500 to-purple-500
       text-white
-    `}>
+    `}
+    >
       <Layout titulo="Cadastro Simples">
-        <div className="flex justify-end">
-          <Botao cor="green" className="mb-4">Novo Cliente</Botao>
-        </div>
+        {visivel === "tabela" ? (
+          <>
+            <div className="flex justify-end">
+              <Botao
+                cor="green"
+                className="mb-4"
+                onClick={novoCliente}
+              >
+                Novo Cliente
+              </Botao>
+            </div>
 
-        <Tabela
-          clientes={clientes}
-          clienteSelecionado={clienteSelecionado}
-          clienteExcluido={clienteExcluido}
-        />
-
-        <Formulario cliente={clientes[0]} />
+            <Tabela
+              clientes={clientes}
+              clienteSelecionado={clienteSelecionado}
+              clienteExcluido={clienteExcluido}
+            />
+          </>
+        ) : (
+          <>
+            <Formulario
+              cliente={cliente}
+              cancelado={() => setVisivel("tabela")}
+              clienteMudou={salvarCliente}
+            />
+          </>
+        )}
       </Layout>
     </main>
-  )
+  );
 }
